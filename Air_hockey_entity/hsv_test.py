c1e8fa74cfbb43117b8496091d55b9388a2ddb66
40623128 <40623128@gm.nfu.edu.tw>
2020-02-19 16:33:06 +0800

    add arduino connect and edit air_hockey_entity main.py

---
+++ Air_hockey_entity/hsv_test.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import array
+
+cap = cv2.VideoCapture(0)
+'''
+range = 10
+lower_blue = np.array([120-range,150,150])
+upper_blue = np.array([120+range,255,255])
+
+range = 10
+lower_green = np.array([60-range,100,100])
+upper_green = np.array([60+range,255,255])
+'''
+
+if __name__ == '__main__':
+    while(True):
+        ret, frame= cap.read()
+        # Our operations on the frame come here
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        range = 10
+        lower_color = np.array([60-range,100,100])
+        upper_color = np.array([60+range,255,255])
+        mask = cv2.inRange(hsv, lower_color, upper_color)
+        # Display the resulting frame
+        cv2.imshow('frame',mask)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
--- Air_hockey_entity/main.py
+++ Air_hockey_entity/main.py
@@ -1,10 +1,22 @@
 import numpy as np
 import array
 import cv2
+import time
 import ColorRecognition as color
-
+import serial
+import sys
+'''
+gamemode
+1 = setting
+2 = play
+'''
+gamemode = 1
+COM_PORT = 'COM5'  # 請自行修改序列埠名稱
+BAUD_RATES = 9600
+ser = serial.Serial(COM_PORT, BAUD_RATES)
+blue_x = None
+blue_y = None
 cap = cv2.VideoCapture(0)
-
 if __name__ == '__main__':
     while(True):
         # Capture frame-by-frame
@@ -14,27 +26,33 @@ if __name__ == '__main__':
         #img2 = numpy.asarray(image_buffer)
         #ColorRecognition
         ret_green = color.track_green_object(frame)
-        ret_red = color.track_red_object(frame)
         ret_blue = color.track_blue_object(frame)
         if ret_blue:
-            for i in range(len(ret_blue)):
-                cv2.rectangle(frame,(int(ret_blue[i][0] - 10),int(ret_blue[i][1] - 10)), (int(ret_blue[i][0] + 10),int(ret_blue[i][1] + 10)), (0,255,0), 2)
-        '''
-        if ret_red:
-            cv2.rectangle( frame, (ret_red[0]-10, ret_red[1] - 10), (ret_red[0]+10,ret_red[1]+10), (255,0,0), 2)
-            print('find red')
-            
+            cv2.rectangle(frame,(ret_blue[0] - 10,ret_blue[1]-10), (ret_blue[0] + 10,ret_blue[1] + 10), (0,255,0), 2)
+            blue_x = ret_blue[0]
+            blue_y = ret_blue[1]
+            ser.write(b'BLUE_ON\n')
+            #print('blue =>(', blue_x, ',', blue_y, ')')
+        else:
+            print('unfind blue')
+            ser.write(b'BLUE_OFF\n')
         if ret_green:
             cv2.rectangle( frame, (ret_green[0]-10, ret_green[1]-10), (ret_green[0]+10,ret_green[1]+10), (0,0,255), 2)
-            print('find green')
-        '''
-        # Our operations on the frame come here
-
-        # Display the resulting frame
+            green_x = ret_green[0]
+            green_y = ret_green[1]
+            ser.write(b'GREEN_ON\n')
+            print('green =>(', green_x, ',', green_y, ')')
+        else:
+            print('unfind green')
+            ser.write(b'GREEN_OFF\n')
+        while ser.in_waiting:
+            mcu_feedback = ser.readline().decode()  # 接收回應訊息並解碼
+            print('Arduino feedback :', mcu_feedback)
+        if gamemode ==1:
+            print('blue =>(', blue_x, ',', blue_y, ')')
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
