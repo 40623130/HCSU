e48a15e3686d57eb98c7b1a5f53265994b54ca90
40623130 <40623130@gm.nfu.edu.tw>
2020-02-21 16:32:51 +0800

    Edit_LOG-22 & Air_Hocky_Arduino_main_program

---
+++ Air_Hockey_Robot_Arduino/Configuration.h
@@ -0,0 +1,7 @@
+#define EN 8      //步進馬達使能端，低電位有效
+#define X_DIR 5   //X軸 步進馬達方向控制
+#define Y_DIR 6   //y軸 步進馬達方向控制
+#define YY_DIR 7  //y軸 步進馬達方向控制
+#define X_STP 2   //x軸 步進控制
+#define Y_STP 3   //y軸 步進控制
+#define YY_STP 4  //y軸 步進控制
